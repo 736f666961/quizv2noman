c75074adb2f2a694859393ba25b4813bbd82f0ba
ABDERRAZZAKELGHYATI <abdrazakghiyati2@gmail.com>
2021-01-07 15:52:19 -0800

    Abderrazzak

---
+++ Ex1/src/application/Player.java
@@ -0,0 +1,22 @@
+package application;
+
+import java.io.FileInputStream;
+
+public class Player {
+	
+	public Player() {
+		
+	}
+	
+	public Player(FileInputStream fileInputStream) {
+		
+	}
+	
+	public void play(int frames) {
+		
+	}
+	
+	public void close() {
+		
+	}
+}
